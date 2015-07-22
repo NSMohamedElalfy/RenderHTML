59f6b530a150c3283b9c74c8d3bf225c16ff7ad9
Mohamed El-Alfy <mo7amedelalfy@gmail.com>
2015-07-22 05:27:21 +0200

    first Commit

--- RehnderHTML/ViewController.swift
+++ RehnderHTML/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    var baseURL = "http://www.assmt.com/json_1.1/get.aspx"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request(.GET, baseURL , parameters: ["type": "01"])
+            .responseString { request , _, string , _ in
+                
+                    var bodyContent:String {
+                        var fristContent = string!.substringFromIndex(advance(string!.startIndex,41))
+                        var lastContent = fristContent.substringToIndex(advance(fristContent.startIndex,51739))
+                        return lastContent
+                    }
+                
+                dispatch_async(dispatch_get_main_queue()){
+                    var content = String(htmlEncodedString: bodyContent)
+                    var html = NSMutableString(string: "<!DOCTYPE html><html><head><title></title></head><body style=\"background:transparent;\">")
+                    html.appendString(content)
+                    html.appendString("</body></html>")
+                    self.webView.loadHTMLString(html.description, baseURL: nil)
+                }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +44,17 @@ class ViewController: UIViewController {
 
 
 }
+
+extension String {
+    init(htmlEncodedString: String) {
+        let encodedData = htmlEncodedString.dataUsingEncoding(NSUTF8StringEncoding)!
+        let attributedOptions : [String: AnyObject] = [
+            NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+            NSCharacterEncodingDocumentAttribute: NSUTF8StringEncoding
+        ]
+        let attributedString = NSAttributedString(data: encodedData, options: attributedOptions, documentAttributes: nil, error: nil)!
+        self.init(attributedString.string)
+    }
+}
+
 
